6bd0a1e864d072bdae1b2541a38457038ba8a9f4
OmerCanKarli <omercankarli1999@gmail.com>
2023-10-03 19:05:31 +0300

    Update

---
+++ flask-python-dependency.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install python3 -y
+sudo yum install python3-pip
+sudo pip3 install flask
+sudo pip3 install flask-mysql
+sudo pip3 install sqlalchemy
+sudo pip3 install Flask-SQLAlchemy
+sudo pip install Flask==2.3.3
